8db0d59407fb2cce6538ed919bc0bb706578d993
czechumennakompie@gmail.com <czechumennakompie@gmail.com>
2022-04-07 16:09:07 +0200

    added ErrorHandlingMiddleware
    added ErrorHandlingMiddleware for error handling, and added validators to dependency container

---
+++ ProductManagementAPI/ProductManagementAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using ProductManagementAPI.Exceptions;
+
+namespace ProductManagementAPI.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public ErrorHandlingMiddleware()
+        {
+
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var response = context.Response;
+
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (ProductNotFoundException productNotFoundException)
+            {
+                response.StatusCode = StatusCodes.Status404NotFound;
+                await response.WriteAsync(productNotFoundException.Message);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = StatusCodes.Status404NotFound;
+                await response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
--- ProductManagementAPI/ProductManagementAPI/Program.cs
+++ ProductManagementAPI/ProductManagementAPI/Program.cs
@@ -1,6 +1,10 @@
+using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using ProductManagementAPI.Entities;
+using ProductManagementAPI.Middleware;
+using ProductManagementAPI.Models;
+using ProductManagementAPI.Models.Validators;
 using ProductManagementAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,9 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddDbContext<ProductManagementDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductManagementDbConnection")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ProductManagementDbSeeder>();
+builder.Services.AddScoped<IValidator<NewProductDto>, NewProductDtoValidator>();
+builder.Services.AddScoped<IValidator<UpdateProductDto>, UpdateProductDtoValidator>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 var app = builder.Build();
 
@@ -19,7 +26,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 SeedDatabase();
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
 app.UseHttpsRedirection();
+
 
 app.UseAuthorization();
 
